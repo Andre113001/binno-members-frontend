8dc9b1cf1680872813128fda9e9d4ba31079464c
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-01-11 02:29:20 +0800

    redesigned and modified dropbox component

---
+++ src/components/dropbox/DropBox.jsx
@@ -0,0 +1,80 @@
+import { useRef, useState } from "react"
+import React from 'react'
+import styles from './dropBox.module.css'
+import ImageRoundedIcon from '@mui/icons-material/ImageRounded';
+
+
+function DropBox() {
+    const [uploadedFile, setUploadedFile] = useState()
+    const [uploadError, setUploadError] = useState(null)
+    const fileRef = useRef()
+
+    const handleFileUpload = async (e) => {
+        const file = e.target.files[0]
+
+        const isImage = file.type.split('/')[0] === 'image' ?? false
+
+        if (!isImage) {
+            setUploadError('The uploaded file is not a valid image.')
+            return
+        }
+
+        if (!checkFileSize(file)) {
+            setUploadError(
+                'The image size exceeds the maximum allowed size of 5 MB.'
+            )
+            return
+        }
+
+        setUploadedFile(file)
+        setUploadError(null)
+
+        return []
+    }
+
+    const checkFileSize = (file) => {
+        const fileSizeInMB = file.size / (1024 * 1024)
+
+        if (fileSizeInMB > 5) {
+            setUploadError(
+                'The file size is too large. Please upload a file that is smaller than 5MB.'
+            )
+            return false
+        }
+
+        return true
+    }
+    
+  return (
+    <>
+        <div className={styles['imageContainer']} onClick={() => fileRef.current.click()}>
+                {!uploadedFile && <div className={styles['TextImage']}>
+                    <ImageRoundedIcon />
+                    <p>Click to upload a photo</p>
+                </div>
+                }
+                
+                {uploadedFile && (
+                        <div className={styles['uploadedPreview']}>
+                            <img
+                                src={URL.createObjectURL(uploadedFile)}
+                                alt="uploaded_image"
+                            />
+                            <span>Click to replace image</span>
+                        </div>
+                )}
+            
+         </div>
+
+         <input
+                ref={fileRef}
+                onChange={handleFileUpload}
+                type="file"
+                style={{ display: 'none' }}
+                accept=".png, .jpg, .jpeg"
+            />
+    </>
+  )
+}
+
+export default DropBox
