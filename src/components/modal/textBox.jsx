b1434c19e1491e7ce3e5a0e8103e6334aca9558f
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-01-11 00:16:15 +0800

    created new textbox for modal

--- src/components/modal/textBox.jsx
+++ src/components/modal/textBox.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { TextareaAutosize } from '@mui/base/TextareaAutosize';
 
 
 export default function textBox() {
@@ -17,12 +16,14 @@ export default function textBox() {
           autoComplete="off"
           >
             <div>
-              <TextareaAutosize
+              <TextField
                 aria-label="empty textarea"
                 placeholder="Write a short description"
-                style={{ maxHeight: '250px' ,height: '150px',maxWidth:'1300px' ,width: '100%', 
-                  margin: "20px", border:"rgb(241,241,241)", backgroundColor:"rgb(241,241,241)"}}
-                minRows={4}
+                multiline
+                style={{ maxHeight: '250px' ,height: '250px',maxWidth:'1300px' ,width: '100%', 
+                  margin: "20px", border:"rgb(241,241,241)", backgroundColor:"rgb(241,241,241)", outline:'none'}}
+                minRows={7}
+                maxRows={9}
               />
             </div>
           </Box>
