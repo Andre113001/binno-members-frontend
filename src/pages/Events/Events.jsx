14145e7e54787d1798d5e96bf4b0a4f6379867ff
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-01-11 18:40:14 +0800

    implemented new modal for event page

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,8 +1,8 @@
 import EventCards from './EventCards.jsx'
 import SideBar from '../../components/Sidebar/Sidebar.jsx';
 import Header from '../../components/header/Header.jsx';
-import Modal from '../../components/modal/modal.jsx'
 import styles from './Events.module.css'
+import NewEventModal from '../../components/newEventModal/newEventModal.jsx';
 
 function Events() {
 
@@ -18,7 +18,7 @@ function Events() {
                 <h1>My Events</h1>
                   <div className={styles["blogButtons"]}> {/*create button container*/}
                     <div className={styles["modalButton"]}>
-                      <Modal />
+                      <NewEventModal />
                     </div>
                   </div>
                   <div className={styles["modalButton"]}> {/*conntent section container*/}
