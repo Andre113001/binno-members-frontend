b3e696028133bfa2dec984558db9e0fc5c846a35
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-01-12 02:39:40 +0800

    Created new Modal for post

---
+++ src/components/newPostModal/PostModalTextBox.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+
+export default function PostModalTextBox() {
+  return (
+    <div>
+        <div>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '1250px' },
+          }}
+          noValidate
+          autoComplete="off"
+          >
+            <div>
+              <TextField
+                aria-label="empty textarea"
+                placeholder="Write a short description"
+                multiline
+                style={{ maxHeight: '250px',maxWidth:'1300px' ,width: '100%', border: 'white', 
+                  margin: "20px", backgroundColor:"rgb(241,241,241)", outline:'none'}}
+                minRows={4}
+                maxRows={5}
+              />
+            </div>
+          </Box>
+        </div>
+    </div>
+  )
+}
