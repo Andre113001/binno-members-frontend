a1ab39a3f632e305101ab8989620b03f3736e223
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-01-13 03:13:03 +0800

    created handlechange and component for editing in conditional rendering

--- src/pages/EnablerAccount/AccountComponents/EnablerInformationTab/InformationTab.jsx
+++ src/pages/EnablerAccount/AccountComponents/EnablerInformationTab/InformationTab.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { companyInformation } from '../../../../assets/companyInfo'
+import React, { useState } from 'react'
 import styles from './informationTab.module.css'
 
 import FmdGoodRoundedIcon from '@mui/icons-material/FmdGoodRounded';
@@ -7,22 +6,100 @@ import CallRoundedIcon from '@mui/icons-material/CallRounded';
 import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
 function InformationTab(props) {
+  const [name, setName] = useState('');
+
+  const handleDescriptionChange = (event) => {
+    props.onDescriptionChange(event.target.value);
+  };
+
+  const handleAddressChange = (event) => {
+    props.onAddressChange(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    props.onEmailChange(event.target.value);
+  };
+
+  const handlePhoneChange = (event) => {
+    props.onPhoneChange(event.target.value);
+  };
+
+  const handleFBChange = (event) => {
+    props.onFBChange(event.target.value);
+  };
+
   return (
     <>
-        <div className={styles["AboutTabContainer"]}> {/* {styles["companyContact"]} */}
-            <h1>About</h1>
-            <hr />
-            <div className={styles["companyContent"]}>
-               <p className={styles["companyDescription"]}>{props.description}</p>
-               <p className={styles["companyLocation"]}><FmdGoodRoundedIcon /> {props.address}</p>
-               <div className={styles["companyContact"]} >
-                    <p><EmailRoundedIcon />{props.email}</p>
-                    <p><CallRoundedIcon />{props.phone}</p>
-                    <p><FacebookRoundedIcon/>@{props.fb}</p>
+        
+            {props.isEditing ?(
+              <div className={styles["editingContainer"]}>
+                <h1>Edit About Page</h1>
+                  <hr />
+                  <Box
+                    component="form"
+                    sx={{ m: 1, width: '60%' , display: 'flex', flexDirection: 'column'}}
+                    noValidate
+                    autoComplete="off"
+                  >
+                    <TextField
+                      id="enablerDescription"
+                      label="Description"
+                      defaultValue={props.description}
+                      onChange={handleDescriptionChange}
+                      sx={{margin: '10px'}}
+                      multiline
+                      maxRows={5}
+                    />
+                      <TextField
+                      id="enablerAddress"
+                      label="Address"
+                      defaultValue={props.address}
+                      onChange={handleAddressChange}
+                      sx={{margin: '10px'}}
+                      />
+                        <TextField
+                        id="enablerEmail"
+                        label="Email"
+                        defaultValue={props.email}
+                        onChange={handleEmailChange}
+                        sx={{margin: '10px'}}
+                        />
+                          <TextField
+                          id="enablerPhone"
+                          label="Phone"
+                          defaultValue={props.phone}
+                          onChange={handlePhoneChange}
+                          sx={{margin: '10px'}}
+                          />
+                          <TextField
+                          id="enablerFacebook"
+                          label="Facebook"
+                          defaultValue={props.fb}
+                          onChange={handleFBChange}
+                          sx={{margin: '10px'}}
+                          />
+                  </Box>
+              </div>
+               ) : (
+                <div className={styles["AboutTabContainer"]}> {/* {styles["enablerContact"]} */}
+                  <h1>About</h1>
+                  <hr />
+                  <div className={styles["enablerContent"]}>
+                      <p className={styles["enablerDescription"]}>{props.description}</p>
+                      <p className={styles["enablerLocation"]}><FmdGoodRoundedIcon /> {props.address}</p>
+                      <div className={styles["enablerContact"]} >
+                          <p><EmailRoundedIcon />{props.email}</p>
+                          <p><CallRoundedIcon />{props.phone}</p>
+                          <p><FacebookRoundedIcon/>@{props.fb}</p>
+                      </div>
+                  </div>
                </div>
-            </div>
-        </div>
+               )} 
+        
     </>
   )
 }
