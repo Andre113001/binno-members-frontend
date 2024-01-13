4107b8a1b33f2468ba7c9d1a7cc37b4ef32089f2
Ted30 <teddymarcenaje@gmail.com>
2024-01-14 04:48:18 +0800

    refetched refactor

--- src/pages/Guides/GuidesCards.jsx
+++ src/pages/Guides/GuidesCards.jsx
@@ -1,60 +1,74 @@
 import React, { useState, useEffect } from 'react'
 import styles from './GuidesCards.module.css'
-import useLoadProfile from '../../hooks/useLoadProfile.jsx';
-import Moment from 'react-moment';
-import { Link } from 'react-router-dom';
-import useAccessToken from '../../hooks/useAccessToken.jsx';
+import useLoadProfile from '../../hooks/useLoadProfile.jsx'
+import Moment from 'react-moment'
+import { Link } from 'react-router-dom'
+import useAccessToken from '../../hooks/useAccessToken.jsx'
 
 const GuideCards = () => {
-    const [guides, setGuides] = useState([]);
-    const { profileData } = useLoadProfile();
-    const accessToken = useAccessToken();
+    const [guides, setGuides] = useState([])
+    const { profileData } = useLoadProfile()
+    const accessToken = useAccessToken()
 
     useEffect(() => {
         const loadHeadingData = async () => {
             if (profileData) {
-                const profile = await profileData;
-                const fetchGuides = await fetch(`/api/program/all/${profile.member_id}`,{
-                    headers: {
-                      Authorization: `Bearer ${accessToken}`
+                const profile = await profileData
+                const fetchGuides = await fetch(
+                    `https://binno-members-repo-production-b8c4.up.railway.app/api/programs/user/${profile.member_id}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${accessToken}`,
+                        },
                     }
-                  });
-                fetchGuides.json().then(result => {
+                )
+                fetchGuides.json().then((result) => {
                     setGuides(result)
                 })
             }
         }
 
-        loadHeadingData();
+        loadHeadingData()
     }, [profileData])
 
     // console.log(guides);
 
     return (
         <>
-            <section className={styles["content"]}>
-                <div className={styles["grid2"]}>
-                    {guides.map((guide)=> (
-                        <Link to={`/guides/${guide.program_id}`} key={guide.program_id} style={{textDecoration: 'none', color: 'inherit'}}>
-                            <div className={styles["guideContent"]} >
+            <section className={styles['content']}>
+                <div className={styles['grid2']}>
+                    {guides.map((guide) => (
+                        <Link
+                            to={`/guides/${guide.program_id}`}
+                            key={guide.program_id}
+                            style={{ textDecoration: 'none', color: 'inherit' }}
+                        >
+                            <div className={styles['guideContent']}>
                                 {/* <Link to={`/`}></Link> */}
-                                <div className={styles["guideImage"]}>
-                                <img src='' alt=''/>
+                                <div className={styles['guideImage']}>
+                                    <img src="" alt="" />
                                 </div>
-                                <div className={styles["guideFooter"]}>
-                                    <div className={styles["TitleDateContainer"]} >
+                                <div className={styles['guideFooter']}>
+                                    <div
+                                        className={styles['TitleDateContainer']}
+                                    >
                                         <h2>{guide.program_heading}</h2>
-                                        <span className={styles["guideDate"]}>Last accessed: <Moment >{guide.program_datemodified}</Moment></span>
-                                        </div> 
-                                        <p>Click to View and Edit</p>
+                                        <span className={styles['guideDate']}>
+                                            Last accessed:{' '}
+                                            <Moment>
+                                                {guide.program_datemodified}
+                                            </Moment>
+                                        </span>
+                                    </div>
+                                    <p>Click to View and Edit</p>
                                 </div>
-                            </div> 
+                            </div>
                         </Link>
-                    ))} 
+                    ))}
                 </div>
             </section>
         </>
-    ) 
+    )
 }
 
-export default GuideCards;
+export default GuideCards
