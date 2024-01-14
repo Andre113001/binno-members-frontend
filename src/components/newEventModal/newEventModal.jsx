084fde5a50542bc9711e6606b8ce3d4ea7d024d8
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-01-15 04:23:33 +0800

    resized event modal

--- src/components/newEventModal/newEventModal.jsx
+++ src/components/newEventModal/newEventModal.jsx
@@ -9,19 +9,10 @@ import AddRoundedIcon from '@mui/icons-material/AddRounded';
 
 export default function NewEventModal() {
     const [currentDate, setCurrentDate] = useState(new Date());
+    const [isEditActive, setIsEditActive] = useState(true)
 
-    useEffect(()=> { 
-      const interValId = setInterval(() => {
-        setCurrentDate(new Date());
-      }, 1000);
-    
-      return () => clearInterval(interValId);
-
-    }, []);
-    
-    const formatDateToText = () => {
-      const dateYear = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-      return currentDate.toLocaleDateString(undefined, dateYear);
+    const toggleEdit = () => {
+      setIsEditActive((prev) => !prev);
     };
 
     const [modal, setModal] = useState(false);
@@ -50,64 +41,61 @@ export default function NewEventModal() {
     <button onClick={toggleModal} className="createEventBtn">
             <AddRoundedIcon/> Create new event
         </button>
-
-        {modal && (
-            <div className="eventModal">
-              <div onClick={toggleModal} className="overlay"></div>
-                <div className="eventModalContent" onSubmit={handleSubmit}>
-                    <div className="titleDateContainer">
-                        <input 
-                          type="text"
-                          className='titleTextBox'
-                          placeholder='Create new event' 
-                          // value={eventTitle}
-                          // onChange={(e) => setEventTitle(e.target.value)}
-                        />
-                        <p>{formatDateToText()}</p>
-                    </div>
-
-                    <button className="close-modal" onClick={toggleModal}>
-                    <CloseRoundedIcon />
-                    </button>
-                    <div className="datepickerContainer">
-                        <PickDate />
-                        <div className="TextBoxContainer">
-                          <Box
-                              component="form"
-                              sx={{
-                                '& .MuiTextField-root': { m: 1, width: '1250px' },
-                              }}
-                              noValidate
-                              autoComplete="off"
-                              >
-                                <div>
-                                  <TextField
-                                    aria-label="empty textarea"
-                                    placeholder="Write a short description"
-                                    multiline
-                                    style={{ maxHeight: '250px' ,maxWidth:'1300px' ,width: '100%', border: 'rgb(241,241,241)', 
-                                      margin: "15px 5px", backgroundColor:"rgb(241,241,241)", outline:'none'}}
-                                    minRows={3}
-                                    maxRows={5}
-                                    // value={eventDescription}
-                                    // onChange={(e) => setEventDescription(e.target.value)}
-                                    inputProps={{ maxLength: 300 }}
-                                  />
-                                </div>
-                              </Box>
+          {modal && (
+              <div className="eventModal">
+                <div onClick={toggleModal} className="overlay"></div>
+                  <div className="eventModalContent" onSubmit={handleSubmit}>
+                      <div className="titleDateContainer">
+                          <input 
+                            type="text"
+                            className='titleTextBox'
+                            placeholder='Create new event' 
+                            // value={eventTitle}
+                            // onChange={(e) => setEventTitle(e.target.value)}
+                          />
+                      </div>
+                      <button className="close-modal" onClick={toggleModal}>
+                      <CloseRoundedIcon />
+                      </button>
+                      <div className="datepickerContainer">
+                          <PickDate />
                         </div>
-                    </div>
-
-                    <div className="eventModalDropboxContainer">
-                      <DropBox />
-                    </div>
-            
-                    <button className='uploadButton' type='submit' onClick={toggleModal}> 
-                      Upload
-                    </button>
-                </div>
-          </div>
-        )}
+                          <div className="TextBoxContainer">
+                            <Box
+                                component="form"
+                                sx={{
+                                  '& .MuiTextField-root': { m: 1, width: '1250px' },
+                                }}
+                                noValidate
+                                autoComplete="off"
+                                >
+                                  <div>
+                                    <TextField
+                                      aria-label="empty textarea"
+                                      placeholder="Write a short description"
+                                      multiline
+                                      style={{ maxHeight: '250px' ,width: '97.5%', border: 'rgb(241,241,241)', 
+                                        margin: "15px 10px", backgroundColor:"rgb(241,241,241)", outline:'none'}}
+                                      minRows={3}
+                                      maxRows={5}
+                                      // value={eventDescription}
+                                      // onChange={(e) => setEventDescription(e.target.value)}
+                                      inputProps={{ maxLength: 300 }}
+                                    />
+                                  </div>
+                                </Box>
+                          </div>
+                      
+                      <div className="eventModalDropboxContainer">
+                        <DropBox />
+                      </div>
+              
+                      <button className='uploadButton' type='submit' onClick={toggleModal}> 
+                        Upload
+                      </button>
+                  </div>
+            </div>
+          )}
     </>
   )
 }
