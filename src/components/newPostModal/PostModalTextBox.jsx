e5656c511fa54b8d6a0dca7054dfe55c84bfa122
Ted30 <teddymarcenaje@gmail.com>
2024-01-16 03:29:30 +0800

    removed multi image upload

--- src/components/newPostModal/PostModalTextBox.jsx
+++ src/components/newPostModal/PostModalTextBox.jsx
@@ -1,33 +1,39 @@
 import { useEffect, useRef, useState } from 'react'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
 
 export default function PostModalTextBox() {
-  return (
-    <div>
+    return (
         <div>
-        <Box
-          component="form"
-          sx={{
-            '& .MuiTextField-root': { m: 1, width: '1250px' },
-          }}
-          noValidate
-          autoComplete="off"
-          >
             <div>
-              <TextField
-                aria-label="empty textarea"
-                placeholder="Write a short description"
-                multiline
-                style={{ maxHeight: '250px',maxWidth:'1300px' ,width: '100%', border: 'white', 
-                  margin: "20px", backgroundColor:"rgb(241,241,241)", outline:'none'}}
-                minRows={4}
-                maxRows={5}
-              />
+                <Box
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '1250px' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <div>
+                        <TextField
+                            aria-label="empty textarea"
+                            placeholder="Write a short description"
+                            multiline
+                            style={{
+                                maxHeight: '250px',
+                                maxWidth: '1200px',
+                                width: '98%',
+                                border: 'white',
+                                margin: '10px',
+                                backgroundColor: 'rgb(241,241,241)',
+                                outline: 'none',
+                            }}
+                            minRows={4}
+                            maxRows={5}
+                        />
+                    </div>
+                </Box>
             </div>
-          </Box>
         </div>
-    </div>
-  )
+    )
 }
--- src/components/newPostModal/newPostModal.jsx
+++ src/components/newPostModal/newPostModal.jsx
@@ -1,76 +1,89 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-import styles from "./newPostModal.module.css"
-import MultiImageUpload from '../multiImageUpload/MultiImageUpload.jsx'
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
+import styles from './newPostModal.module.css'
 import PostModalTextBox from './PostModalTextBox'
 import PostCategory from './Categories.jsx'
+import DropBox from '../dropbox/DropBox.jsx'
 
 export default function NewPostModal() {
-  const [currentDate, setCurrentDate] = useState(new Date());
+    const [currentDate, setCurrentDate] = useState(new Date())
 
-  useEffect(()=> { 
-    const interValId = setInterval(() => {
-      setCurrentDate(new Date());
-    }, 1000);
-  
-    return () => clearInterval(interValId);
+    useEffect(() => {
+        const interValId = setInterval(() => {
+            setCurrentDate(new Date())
+        }, 1000)
 
-  }, []);
-  
-  const formatDateToText = () => {
-    const dateYear = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-    return currentDate.toLocaleDateString(undefined, dateYear);
-  }; 
+        return () => clearInterval(interValId)
+    }, [])
 
-    const [modal, setModal] = useState(false);    
+    const formatDateToText = () => {
+        const dateYear = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        }
+        return currentDate.toLocaleDateString(undefined, dateYear)
+    }
+
+    const [modal, setModal] = useState(false)
     const toggleModal = () => {
-      setModal(!modal);
-    };
-  
-    if(modal) {
-      document.body.classList.add('active-modal')
+        setModal(!modal)
+    }
+
+    if (modal) {
+        document.body.classList.add('active-modal')
     } else {
-      document.body.classList.remove('active-modal')
-    }  
-  
+        document.body.classList.remove('active-modal')
+    }
+
     return (
-    <>
-    <button onClick={toggleModal} className={styles["createBlog"]}>
-            Create new post
-        </button>
+        <>
+            <button onClick={toggleModal} className={styles['createBlog']}>
+                Create new post
+            </button>
 
-        {modal && (
-            <div className={styles["modal"]}>
-              <div onClick={toggleModal} className={styles["overlay"]}></div>
-                <div className={styles["modal-content"]}>
-                    <div className={styles["titleContainer"]}>
-                        <input 
-                          type="text"
-                          className={styles["titleTextBox"]}
-                          placeholder='Create new post' 
-                          style={{margin: '10px', outline: 'none'}}
-                        />
-                        <div className={styles["DateCategoryContainer"]}>
-                          <p>{formatDateToText()}</p>
-                        <PostCategory />
+            {modal && (
+                <div className={styles['modal']}>
+                    <div
+                        onClick={toggleModal}
+                        className={styles['overlay']}
+                    ></div>
+                    <div className={styles['modal-content']}>
+                        <div className={styles['titleContainer']}>
+                            <input
+                                type="text"
+                                className={styles['titleTextBox']}
+                                placeholder="Create new post"
+                                style={{ margin: '10px', outline: 'none' }}
+                            />
+                            <div className={styles['DateCategoryContainer']}>
+                                <p>{formatDateToText()}</p>
+                                <PostCategory />
+                            </div>
                         </div>
-                    </div>
 
-                    <button className={styles["close-modal"]} onClick={toggleModal}>
-                    <CloseRoundedIcon />
-                    </button>
-                    <div className={styles["textBoxContainer"]}>
-                      <PostModalTextBox />
+                        <button
+                            className={styles['close-modal']}
+                            onClick={toggleModal}
+                        >
+                            <CloseRoundedIcon />
+                        </button>
+                        <div className={styles['textBoxContainer']}>
+                            <PostModalTextBox />
+                        </div>
+                        <div className={styles['dropboxContainer']}>
+                            <DropBox />
+                        </div>
+                        <button
+                            className={styles['uploadButton']}
+                            onClick={toggleModal}
+                        >
+                            Upload
+                        </button>
                     </div>
-                    <div className={styles["dropboxContainer"]}>
-                      <MultiImageUpload />
-                    </div>
-                    <button className={styles["uploadButton"]} onClick={toggleModal}>  
-                      Upload
-                    </button>
                 </div>
-            </div>
-        )}
-    </>
-  )
+            )}
+        </>
+    )
 }
