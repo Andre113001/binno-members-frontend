c2b541abd89b475aa6bda05834bc5ba5aacb6a0d
Andre113001 <1130marcusa@gmail.com>
2024-01-16 04:42:37 +0800

    for production

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { AccountContextProvider } from './context/accountContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
