c771f3847b17bf100e63784029396056c7d697a0
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-04-16 16:18:28 +0800

    added request tabs

---
+++ src/pages/Mentor/Tabs/RequestTab/Requests_data.jsx
@@ -0,0 +1,22 @@
+const Requests = [
+  {
+    id: "1",
+    mentorName: "Mentor mo ko",
+    mentorProfile:
+      "https://img.freepik.com/premium-photo/portrait-businessman-modern-office_252847-38998.jpg",
+    credentials:
+      "Lorem ipsum dolor sit amet. Non facere eaque sed possimus suscipit cum rerum corporis eos exercitationem exercitationem. Lorem ipsum dolor sit amet. Non facere eaque sed possimus suscipit cum rerum corporis eos exercitationem exercitationem.Lorem ipsum dolor sit amet. Non facere eaque sed possimus suscipit cum rerum corporis eos exercitationem exercitationem.Lorem ipsum dolor sit amet. Non facere eaque sed possimus suscipit cum rerum corporis eos exercitationem exercitationem.",
+    attachedFile: "Profile.pdf",
+  },
+  {
+    id: "2",
+    mentorName: "Mentor mo ko",
+    mentorProfile:
+      "https://img.freepik.com/premium-photo/portrait-businessman-modern-office_252847-38998.jpg",
+    credentials:
+      "Lorem ipsum dolor sit amet. Non facere eaque sed possimus suscipit cum rerum corporis eos exercitationem exercitationem. Lorem ipsum dolor sit amet. Non facere eaque sed possimus suscipit cum rerum corporis eos exercitationem exercitationem.Lorem ipsum dolor sit amet. Non facere eaque sed possimus suscipit cum rerum corporis eos exercitationem exercitationem.Lorem ipsum dolor sit amet. Non facere eaque sed possimus suscipit cum rerum corporis eos exercitationem exercitationem.",
+    attachedFile: "Profile.pdf",
+  },
+];
+
+export default Requests;
